d6f04c14a41d47e962f7082d877bd0c630a909a1
maissambaye <maissa.mbaye@ugb.edu.sn>
2022-12-28 17:40:08 +0000

    mise à jours de l'horloge

--- p004_horloge/lib/main.dart
+++ p004_horloge/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,9 +26,9 @@ class Horloge extends StatefulWidget {
 }
 
 class _HorlogeState extends State<Horloge> {
-  String _heureCourante = "00";
-  String _minuteCourante = "00";
-  String _secondeCourante = "00";
+  String _hCourante = "00";
+  String _mCourante = "00";
+  String _sCourante = "00";
   late Timer _timerHorloge;
   bool _startHorloge = false;
 
@@ -35,9 +36,9 @@ class _HorlogeState extends State<Horloge> {
     if (_startHorloge == true) {
       _timerHorloge = Timer.periodic(Duration(seconds: 1), (timer) {
         setState(() {
-          _heureCourante = DateTime.now().hour.toString().padLeft(2, '0');
-          _minuteCourante = DateTime.now().minute.toString().padLeft(2, '0');
-          _secondeCourante = DateTime.now().second.toString().padLeft(2, '0');
+          _hCourante = DateTime.now().hour.toString().padLeft(2, '0');
+          _mCourante = DateTime.now().minute.toString().padLeft(2, '0');
+          _sCourante = DateTime.now().second.toString().padLeft(2, '0');
         });
       });
     }
@@ -45,86 +46,74 @@ class _HorlogeState extends State<Horloge> {
 
   void arreterHorloge() {
     if (_startHorloge == false) {
-      _timerHorloge!.cancel();
+      _timerHorloge.cancel();
     }
-  }
-
-  void resetHorloge() {
-    setState(() {
-      _heureCourante = "00";
-      _minuteCourante = "00";
-      _secondeCourante = "00";
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.blueGrey,
       body: Center(
         child: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-                begin: Alignment.topLeft, end: Alignment.bottomRight,
-                //      colors: [Colors.orange, Colors.green]),
-                //colors: [Colors.orange, Colors.purple]),
-                //colors: [Colors.lightBlue, Colors.purple]),
-                // colors: [Colors.lightBlue, Colors.purple, Colors.orange]),
-                // colors: [Colors.lightBlue, Colors.purple, Colors.orange]),
-                colors: [Color(0xFFe66465), Color(0xFF9198e5)]),
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFFe66465),
+                  Color(0xFF9198e5),
+                ]),
           ),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    buildAffichageHorraire(_heureCourante, "heures"),
-                    buildAffichageHorraire(_minuteCourante, "minutes"),
-                    buildAffichageHorraire(_secondeCourante, "secondes"),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _startHorloge = !_startHorloge;
-                          demarrerHorloge();
-                        });
-                      },
-                      child: Icon(
-                        Icons.play_arrow,
-                        size: 25,
-                        color: Colors.white,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        minimumSize: Size(100, 50),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _startHorloge = !_startHorloge;
-                          arreterHorloge();
-                        });
-                      },
-                      child: Icon(
-                        Icons.pause,
-                        size: 25,
-                        color: Colors.white,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.red,
-                          minimumSize: Size(100, 50)),
-                    )
-                  ],
-                )
-              ]),
+          child: buildColonneCentrale(),
         ),
       ),
+    );
+  }
+
+  ElevatedButton buildStartButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_startHorloge == false) {
+          _startHorloge = !_startHorloge;
+          demarrerHorloge();
+        }
+      },
+      child: Icon(
+        Icons.play_arrow,
+        size: 25,
+        color: Colors.white,
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.green,
+        minimumSize: Size(100, 50),
+      ),
+    );
+  }
+
+  ElevatedButton buildPauseButton() {
+    return ElevatedButton(
+      onPressed: () {
+        if (_startHorloge == true) {
+          _startHorloge = !_startHorloge;
+          arreterHorloge();
+        }
+      },
+      child: Icon(
+        Icons.pause,
+        size: 25,
+        color: Colors.white,
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.red,
+        minimumSize: Size(100, 50),
+      ),
+    );
+  }
+
+  Row buildLigneControles() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [buildStartButton(), buildPauseButton()],
     );
   }
 
@@ -134,7 +123,9 @@ class _HorlogeState extends State<Horloge> {
       width: 100,
       margin: EdgeInsets.only(right: 10),
       decoration: BoxDecoration(
-          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -156,6 +147,27 @@ class _HorlogeState extends State<Horloge> {
           )
         ],
       ),
+    );
+  }
+
+  Row buildHeureCourante() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildAffichageHorraire(_hCourante, "heures"),
+        buildAffichageHorraire(_mCourante, "minutes"),
+        buildAffichageHorraire(_sCourante, "secondes"),
+      ],
+    );
+  }
+
+  Column buildColonneCentrale() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        buildHeureCourante(),
+        buildLigneControles(),
+      ],
     );
   }
 }
